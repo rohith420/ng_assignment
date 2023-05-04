80a247b31ee3bd2568c2dc5b1c7c31ea0fd0057c
rohith420 <rohith4560@gmail.com>
2023-05-04 22:50:08 +0530

    dotenv added

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var express = require('express')
 var mongoose = require('mongoose')
 var cors = require('cors')
 var bodyparser = require('body-parser')
+require('dotenv').config()
 var app=express()
 app.use(bodyparser.json())
 
 app.use(cors({
     origin:"*"
 }))
-
+const port = process.env.PORT || 2000
 const connect = ()=>{
-    return mongoose.connect("mongodb+srv://rbjuly31:students@cluster0.15vxajj.mongodb.net/test")
+    return mongoose.connect(`mongodb+srv://rbjuly31:${process.env.PASSWORD}@cluster0.15vxajj.mongodb.net/test`)
 }
 require('./MainRoutes')(app)
-app.listen(2000,()=>{
+app.listen(port,()=>{
     connect()
 })
