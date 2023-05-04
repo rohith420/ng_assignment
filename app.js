d29e32ff5a4dd62fa1710088e08222be7493a5c0
rohith420 <rohith4560@gmail.com>
2023-05-04 23:29:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.use(bodyparser.json())
 app.use(cors({
     origin:"*"
 }))
-// const port = process.env.PORT || 2000
+const port = process.env.PORT || 2000
 const connect = ()=>{
-    return mongoose.connect(`mongodb+srv://rbjuly31:students@cluster0.15vxajj.mongodb.net/test`)
+    return mongoose.connect(`mongodb+srv://rbjuly31:${process.env.PASSWORD}@cluster0.15vxajj.mongodb.net/test`)
 }
 require('./MainRoutes')(app)
-app.listen(2000,()=>{
+app.listen(port,()=>{
     connect()
 })
